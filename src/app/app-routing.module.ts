67d10c030980eb9534507d7bba1972f3b9cd7375
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2023-09-23 16:13:49 +0200

    Moving sections to components
    moving sections to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 
 const routes: Routes = [
   { path: "", component:HomeComponent},
   { path: "register", component:RegisterComponent},
   { path: "contact", component:ContactComponent},
+  { path: "timeline", component:TimelineComponent},
+  { path: "faqs", component:FaqsComponent}, 
+  { path: "faqs", component:FaqsComponent}, 
+  { path: "overview", component: OverviewComponent}, 
 ];
+  
 
 @NgModule({
   declarations: [],
